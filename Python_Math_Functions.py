080099c8e4bd7b8f15b4e1e8d3b4ca2b4305f2ab
Seahawkkid000 <107086227+Seahawkkid000@users.noreply.github.com>
2023-09-04 18:59:28 -0500

    Add files via upload

---
+++ Python_Math_Functions.py
@@ -0,0 +1,76 @@
+import math
+import cmath
+print("Menu:")
+print("   1. Find The Value Of Pi")
+print("   2. Find The Circumference Of A Circle Using Pi")
+print("   3. Find The Area Of A Circle")
+print("   4. Find The Value Of Tau")
+print("   5. Find The Circumference Of A Circle Using Tau")
+print("   6. Find The Value Of Euler's Number")
+print("   7. Find The Value Of Infinity")
+print("   8. Find The Value Of Negative Infinity")
+print("   9. Figure Out If A Value Is More Than Infinity")
+print("   10. Figure Out If A Value Is Less Than Negative Infinity")
+print("   11. Find The Factorial Of A Number Using A For Loop")
+print("   12. Find The Factorial Of A Number Using A Recursive Function")
+print("   13. Find The Factorial Of A Number Using A Built-In Function")
+print("   14. Figure Out How Long It Takes For Each Way Of Finding A Factorial")
+print("   15. Find Out The Smallest Value An Integer Can Be To Be Greater Than Or Equal To A Number")
+print("   16. Find Out The Biggest Value An Integer Can Be To Be Less Than Or Equal To A Number")
+print("   17. Delete The Decimal Of A Number")
+print("   18. See If Two Numbers Are Close For Big Values")
+print("   19. See If Two Numbers Are Close For Small Values")
+print("   20. Find The Power Of A Number Using Asterisks")
+print("   21. Find The Power Of A Number Using A Built-In Function")
+print("   23. Find The Power Of A Number Using A Different Built-In Function")
+print("   24. Find How Much Time It Takes For Each Of The Methods To Find The Power Of A Number") 
+print("   25. Find The Natural Exponent Of A Number Using Asterisks")
+print("   26. Find The Natural Exponent Of A Number Using The Power Function")
+print("   27. Find The Natural Exponent Of A Number Using A Different Function")
+print("   28. Find How Much Time It Takes For Each Of The Methods To Find The Natural Exponent Of A Number")
+print("   29. Find The Remaing Quantity Of a Radioactive Element After A Certain Number Of Years")
+print("   30. Find The Logarithm Of A Number")
+print("   31. Find The Half-Life Of An Unknown Radioactive Element By Measuring The Mass At An Interval")
+print("   32. Calculate The Greatest Common Factor")
+print("   33. Calculate The Sum Of Iterables Using A Function")
+print("   34. Calculate The Sum Of Iterables Using A  Different Function")
+print("   35. Calculate The Square Root Of A Number")
+print("   36. Convert Angle Values")
+print("   37. Calculate The Sine Value Of An Angle")
+print("   38. Calculate The Cosine Value Of An Angle")
+print("   39. Calculate The Tangent Value Of An Angle")
+print("   40. Calculate The Arc Sine Value Of An Angle")
+print("   41. Calculate The Arc Cosine Value Of An Angle")
+print("   42. Calculate The Arc Tangent Value Of An Angle")
+print("   43. Calculate The Hypotenuse Of A Triangle")
+print("   44. Return The Number Of Ways To Choose A Number Of Items from A Number Of Items Without Repetition And Without Particular Order")
+print("   44. Return The Number Of Ways To Choose A Number Of Items from A Number Of Items Without Repetition And With Order")
+print("   45. Return The Integer Square Root Of A Non-Negative Integer")
+print("   46. Calculate The Product Of All Of The Elements In The Input Iterable")
+print("   47. Return The Euclidean Distance Between Two Points")
+print("   48. See What Type Of Number You Have")
+print("   49. Create A Complex Number")
+while True:
+    choice=int(input("Enter your choice"))
+    if(choice==1):
+        print(math.pi)
+    elif(choice==2):
+        radius=int(input("Enter the radius of the circle")
+        circumference=2*math.pi*radius
+        print("The circumference of your circle is", circumference)
+    elif(choice==3):
+        radius=int(input("Enter the radius of the circle")
+        area=math.pi*r*r
+        print("The area of your circle is", area)
+    elif(choice==4):
+        print(math.tau)
+    elif(choice==5):
+        radius=int(input("Enter the radius of the circle")
+        circumference=math.tau*radius
+        print("The circumference of your circle is", circumference)
+        
+
+
+
+
+
